6cc1a744657ef4123bc7f09ed085f1cb22e8a960
goofansu <goofan.su@gmail.com>
2016-07-28 15:29:37 +0800

    update document

--- lib/wechat/plugs/check_msg_signature.ex
+++ lib/wechat/plugs/check_msg_signature.ex
@@ -15,7 +15,7 @@ defmodule Wechat.Plugs.CheckMsgSignature do
 
 
   @doc """
-  # xml in body
+  # body
   # [{<<"tousername">>,[],[<<"gh_7e071da28932">>]},
   # {<<"fromusername">>,[],[<<"oi00OuKAhA8bm5okpaIDs7WmUZr4">>]},
   # {<<"createtime">>,[],[<<"1468999644">>]},
@@ -24,12 +24,14 @@ defmodule Wechat.Plugs.CheckMsgSignature do
   # {<<"msgid">>,[],[<<"6309305429231578443">>]}]
 
   # return
-  # {content => <<229,147,136>>,
-  # createtime => <<"1468999644">>,
-  # fromusername => <<"oi00OuKAhA8bm5okpaIDs7WmUZr4">>,
-  # msgid => <<"6309305429231578443">>,
-  # msgtype => <<"text">>,
-  # tousername => <<"gh_7e071da28932">>}
+  # %{
+  #   content => <<229,147,136>>,
+  #   createtime => <<"1468999644">>,
+  #   fromusername => <<"oi00OuKAhA8bm5okpaIDs7WmUZr4">>,
+  #   msgid => <<"6309305429231578443">>,
+  #   msgtype => <<"text">>,
+  #   tousername => <<"gh_7e071da28932">>
+  # }
   """
   defp parse_xml(body) do
     [{"xml", [], attrs}] = Floki.find(body, "xml")
