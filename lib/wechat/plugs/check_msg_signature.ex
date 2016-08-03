b7494c6a6f1ffb8dcb71d495dc0bbe75843a7e40
goofansu <goofan.su@gmail.com>
2016-08-03 15:16:40 +0800

    support decrypt msg

--- lib/wechat/plugs/check_msg_signature.ex
+++ lib/wechat/plugs/check_msg_signature.ex
@@ -4,39 +4,62 @@ defmodule Wechat.Plugs.CheckMsgSignature do
   """
 
   import Plug.Conn
+  import Wechat.MsgParser
+  import Wechat.Signature
+  import Wechat.Cipher
 
   def init(opts) do
-    opts
+    Keyword.merge opts,
+      token: Wechat.config[:token],
+      appid: Wechat.config[:appid],
+      aes_key: aes_key(Wechat.config[:encoding_aes_key])
   end
 
-  def call(conn, _opts) do
-    {:ok, body, conn} = read_body(conn)
-    msg = parse_xml(body)
-    assign(conn, :msg, msg)
+  defp aes_key(nil) do
+    nil
+  end
+  defp aes_key(encoding_aes_key) do
+    encoding_aes_key <> "=" |> Base.decode64!
   end
 
-
-  # attrs
-  # [{<<"tousername">>,[],[<<"gh_7e071da28932">>]},
-  # {<<"fromusername">>,[],[<<"oi00OuKAhA8bm5okpaIDs7WmUZr4">>]},
-  # {<<"createtime">>,[],[<<"1468999644">>]},
-  # {<<"msgtype">>,[],[<<"text">>]},
-  # {<<"content">>,[],[<<229,147,136>>]},
-  # {<<"msgid">>,[],[<<"6309305429231578443">>]}]
-
-  # return
-  # %{
-  #   content => <<229,147,136>>,
-  #   createtime => <<"1468999644">>,
-  #   fromusername => <<"oi00OuKAhA8bm5okpaIDs7WmUZr4">>,
-  #   msgid => <<"6309305429231578443">>,
-  #   msgtype => <<"text">>,
-  #   tousername => <<"gh_7e071da28932">>
-  # }
-  defp parse_xml(body) do
-    [{"xml", [], attrs}] = Floki.find(body, "xml")
-    for {key, _, [value]} <- attrs, into: %{} do
-      {String.to_atom(key), value}
+  def call(conn, opts) do
+    {:ok, xml, conn} = read_body(conn)
+    msg = xml |> parse
+    case msg_encrypted?(conn.params) do
+      true -> decrypt_msg(conn, msg, opts)
+      false -> conn |> assign(:msg, msg)
     end
+  end
+
+  defp decrypt_msg(conn, %{encrypt: msg_encrypt} = msg, opts) do
+    token = Keyword.fetch!(opts, :token)
+    aes_key = Keyword.fetch!(opts, :aes_key)
+    case msg_valid?(msg_encrypt, conn.params, token) do
+      true ->
+        case decrypt(msg_encrypt, aes_key) do
+          {appid, msg_decrypt} ->
+            conn
+            |> assign(:msg, msg_decrypt |> parse)
+          _ ->
+            conn
+            |> send_resp(400, "decrypt msg failed")
+            |> halt
+        end
+      false ->
+        conn
+        |> send_resp(400, "invalid msg")
+        |> halt
+    end
+  end
+
+  defp msg_encrypted?(params) do
+    encrypt_type = Map.get(params, "encrypt_type")
+    encrypt_type in [nil, "raw"] == false
+  end
+
+  defp msg_valid?(msg_encrypt, params, token) do
+    %{"timestamp" => timestamp, "nonce" => nonce, "msg_signature" => signature} = params
+    my_signature = [token, timestamp, nonce, msg_encrypt] |> sign
+    my_signature == signature
   end
 end
