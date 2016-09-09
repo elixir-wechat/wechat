8e6479d31fb1a40c6b3fbeb565a1af847c2e2839
Jun Lin <linjunpop@gmail.com>
2016-09-09 17:20:06 +0800

    API to deliver template message.

---
+++ lib/wechat/message/template.ex
@@ -0,0 +1,22 @@
+defmodule Wechat.Message.Template do
+  @moduledoc """
+  Template Message API.
+
+  ref: http://mp.weixin.qq.com/wiki/5/6dde9eaa909f83354e0094dc3ad99e05.html#.E5.8F.91.E9.80.81.E6.A8.A1.E6.9D.BF.E6.B6.88.E6.81.AF
+  """
+
+  import Wechat.ApiBase
+
+  @api_path "message/template/send"
+
+  def send(openid, template_id, url, data) do
+    body = %{
+      "touser" => openid,
+      "template_id" => template_id,
+      "url" => url,
+      "data" => data
+    }
+
+    post @api_path, body
+  end
+end
