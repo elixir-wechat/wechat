fd9fcce536da745ee50f5b3390122613375b4d96
YeJun Su <goofan.su@gmail.com>
2017-07-30 03:24:27 +0800

    read default configs from env

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,8 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+config :wechat, Wechat,
+  appid: {:system, "WECHAT_APPID"},
+  secret: {:system, "WECHAT_SECRET"},
+  token: {:system, "WECHAT_TOKEN"},
+  encoding_aes_key: {:system, "WECHAT_AES_KEY"}
