68158971f20d8edd3bf9432e7144d5edc19118f8
YeJun Su <goofan.su@gmail.com>
2017-07-31 18:33:51 +0800

    add credo and cortex

--- mix.exs
+++ mix.exs
@@ -41,6 +41,8 @@ defmodule Wechat.Mixfile do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:exvcr, "~> 0.8", only: :test},
+      {:credo, "~> 0.3", only: [:dev, :test]},
+      {:cortex, "~> 0.1", only: [:dev, :test]},
       {:httpoison, "~> 0.12"},
       {:poison, "~> 3.1"},
       {:floki, "~> 0.17.0"},
