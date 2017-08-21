ac959c659a94bce5b256cba8628f288923d29e4c
qhwa <qhwa@163.com>
2017-08-21 21:00:50 +0800

    support sending links and weapps as custom message

--- test/wechat/message/custom_test.exs
+++ test/wechat/message/custom_test.exs
@@ -17,4 +17,29 @@ defmodule Wechat.Message.CustomTest do
       assert result["errmsg"] == "ok"
     end
   end
+
+  test "send link", %{openid: openid} do
+    use_cassette "send_link" do
+      result = Custom.send_link(openid, %{
+        title: "Awesome webpage",
+        description: "Click and you will go",
+        url: "http://www.example.com",
+        thumb_url: "https://gitlab.com/gitlab-com/gitlab-artwork/raw/master/logo/logo.png"
+      })
+      assert result["errcode"] == 0
+      assert result["errmsg"] == "ok"
+    end
+  end
+
+  test "send weapp page", %{openid: openid} do
+    use_cassette "send_mini_program_app_page" do
+      result = Custom.send_mini_program_app_page(openid, %{
+        title: "my mini program app",
+        pagepath: "/pages/index?id=1",
+        thumb_media_id: "111"
+      })
+      assert result["errcode"] == 0
+      assert result["errmsg"] == "ok"
+    end
+  end
 end
