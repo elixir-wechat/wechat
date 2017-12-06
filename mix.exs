1e2d3147bc60eea55053312ba690b8664af451d3
YeJun Su <goofan.su@gmail.com>
2017-12-06 21:59:05 +0800

    0.2.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wechat.Mixfile do
   def project do
     [
       app: :wechat,
-      version: "0.2.5",
+      version: "0.2.6",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
