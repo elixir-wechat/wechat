9cdea06f15114e5f550408a17f3c8b8c4281f010
YeJun Su <goofan.su@gmail.com>
2017-12-14 02:12:30 +0800

    fix test

--- test/wechat/access_token_worker_test.exs
+++ test/wechat/access_token_worker_test.exs
@@ -2,29 +2,23 @@ defmodule Wechat.Worker.AccessTokenTest do
   use ExUnit.Case, async: false
 
   alias Wechat.Workers.AccessToken
-  
+
   test "custom fetcher" do
     Application.put_env(
       :wechat,
-      Wechat,
-      [access_token_fetcher: {__MODULE__, :fetch_token, []}]
+      :access_token_fetcher,
+      {__MODULE__, :fetch_token, []}
     )
 
     AccessToken.start_link([])
     Process.send AccessToken, :refresh, []
 
-    assert GenServer.call(AccessToken, :get) == "test" 
+    assert GenServer.call(AccessToken, :get) == "test"
   end
 
   test "setting custom refresh token" do
     interval = :timer.minutes(10)
-
-    Application.put_env(
-      :wechat,
-      Wechat,
-      [access_token_refresh_interval: interval]
-    )
-
+    Application.put_env(:wechat, :access_token_refresh_interval, interval)
     assert AccessToken.refresh_interval == interval
   end
 
@@ -33,4 +27,3 @@ defmodule Wechat.Worker.AccessTokenTest do
   end
 
 end
-
