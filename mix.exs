a6178a46b0d6eca046ec9fd7e832804935e856d0
YeJun Su <goofan.su@gmail.com>
2017-12-14 10:06:25 +0800

    use ebertapp.io to review code

--- mix.exs
+++ mix.exs
@@ -42,7 +42,6 @@ defmodule Wechat.Mixfile do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
-      {:credo, "~> 0.3", only: :dev},
       {:exvcr, "~> 0.8", only: :test},
       {:excoveralls, "~> 0.7", only: :test},
       {:httpoison, "~> 0.13"},
