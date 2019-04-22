e8957600c0dacc4f5aae39c9854d815e4786fc97
YeJun Su <goofan.su@gmail.com>
2019-04-22 14:35:37 +0800

    refactor Wechat.Utils.MessageEncryptor

--- lib/wechat/utils/message_encryptor.ex
+++ lib/wechat/utils/message_encryptor.ex
@@ -3,7 +3,6 @@ defmodule Wechat.Utils.MessageEncryptor do
   Encrypt and decrypt Wechat messages.
   """
 
-  @aes_key_size 32
   @aes_block_size 16
 
   # get AES key from encoding_aes_key.
@@ -18,36 +17,10 @@ defmodule Wechat.Utils.MessageEncryptor do
     with aes_key <- aes_key(encoding_aes_key) do
       msg
       |> pack_appid(appid)
-      |> encode_padding_with_pkcs7
+      |> encode_padding_with_pkcs7(32)
       |> encrypt_with_aes_cbc(aes_key)
       |> Base.encode64()
     end
-  end
-
-  # random(16B) + msg_size(4B) + msg + appid
-  defp pack_appid(msg, appid) do
-    random = :crypto.strong_rand_bytes(16)
-    msg_size = byte_size(msg)
-    random <> <<msg_size::32>> <> msg <> appid
-  end
-
-  defp encode_padding_with_pkcs7(data) do
-    data_size = byte_size(data)
-    size_to_pad = calc_pkcs7_padding_size(data_size)
-    padding = String.duplicate(<<size_to_pad::8>>, size_to_pad)
-    data <> padding
-  end
-
-  defp calc_pkcs7_padding_size(data_size) do
-    case rem(data_size, @aes_key_size) do
-      0 -> @aes_key_size
-      rem -> @aes_key_size - rem
-    end
-  end
-
-  defp encrypt_with_aes_cbc(plain_text, aes_key) do
-    iv = binary_part(aes_key, 0, @aes_block_size)
-    :crypto.block_encrypt(:aes_cbc, aes_key, iv, plain_text)
   end
 
   @doc """
@@ -58,14 +31,27 @@ defmodule Wechat.Utils.MessageEncryptor do
       msg_encrypted
       |> Base.decode64!()
       |> decrypt_with_aes_cbc(aes_key)
-      |> decode_padding_with_pkcs7
-      |> unpack_appid
+      |> decode_padding_with_pkcs7()
+      |> unpack_appid()
     end
   end
 
-  defp decrypt_with_aes_cbc(cipher_text, aes_key) do
-    iv = binary_part(aes_key, 0, @aes_block_size)
-    :crypto.block_decrypt(:aes_cbc, aes_key, iv, cipher_text)
+  # random(16B) + msg_size(4B) + msg + appid
+  defp pack_appid(msg, appid) do
+    random = :crypto.strong_rand_bytes(16)
+    msg_size = byte_size(msg)
+    random <> <<msg_size::32>> <> msg <> appid
+  end
+
+  # random(16B) + msg_size(4B) + msg + appid
+  defp unpack_appid(<<_::binary-16, msg_size::32, msg::binary-size(msg_size), appid::binary>>) do
+    {appid, msg}
+  end
+
+  defp encode_padding_with_pkcs7(data, pad_block_size) do
+    pad = calc_pad(data, pad_block_size)
+    padding = String.duplicate(<<pad::8>>, pad)
+    data <> padding
   end
 
   defp decode_padding_with_pkcs7(data) do
@@ -74,8 +60,22 @@ defmodule Wechat.Utils.MessageEncryptor do
     binary_part(data, 0, data_size - pad)
   end
 
-  # random(16B) + msg_size(4B) + msg + appid
-  defp unpack_appid(<<_::binary-16, msg_size::32, msg::binary-size(msg_size), appid::binary>>) do
-    {appid, msg}
+  defp encrypt_with_aes_cbc(plain_text, aes_key) do
+    iv = binary_part(aes_key, 0, @aes_block_size)
+    :crypto.block_encrypt(:aes_cbc, aes_key, iv, plain_text)
+  end
+
+  defp decrypt_with_aes_cbc(cipher_text, aes_key) do
+    iv = binary_part(aes_key, 0, @aes_block_size)
+    :crypto.block_decrypt(:aes_cbc, aes_key, iv, cipher_text)
+  end
+
+  defp calc_pad(data, pad_block_size) do
+    data_size = byte_size(data)
+
+    case rem(data_size, pad_block_size) do
+      0 -> pad_block_size
+      rem -> pad_block_size - rem
+    end
   end
 end
